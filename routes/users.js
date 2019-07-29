c788ec55c10a897ca596ec74182adecffb9055a7
guilleeh <guilleeh@uci.edu>
2019-07-29 14:07:12 -0700

    Register user error checking

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -23,12 +24,40 @@ router.post(
             'Please enter a password with 6 or more characters.'
         ).isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ erros: errors.array() });
         }
-        res.send('passed');
+
+        const { name, email, password } = req.body;
+
+        try {
+            //find if user already exists by email
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists.' });
+            }
+
+            //create a new instance of a user
+            user = new User({
+                name,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            //assigning a new hashed version of password
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('User saved');
+        } catch (error) {
+            console.error(error.message);
+            res.state(500).json('Server error');
+        }
     }
 );
 
