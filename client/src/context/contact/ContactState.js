32358488d92920608f0c8f87454281723833c7bf
guilleeh <guilleeh@uci.edu>
2019-08-01 14:58:35 -0700

    Add contact form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -49,6 +49,10 @@ const ContactState = props => {
     const [state, dispatch] = useReducer(contactReducer, intialState);
 
     // Add contact
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    };
 
     // Delete contact
 
@@ -65,7 +69,8 @@ const ContactState = props => {
     return (
         <ContactContext.Provider
             value={{
-                contacts: state.contacts
+                contacts: state.contacts,
+                addContact
             }}
         >
             {props.children}
