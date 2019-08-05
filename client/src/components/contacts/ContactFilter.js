5075a037d287294a8ca43ed0ce25e47a9fbd1386
guilleeh <guilleeh@uci.edu>
2019-08-05 13:32:45 -0700

    UI changes based on filtered contacts

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,15 +1,35 @@
-import React, { useContext, useRef } from 'react';
-import contactContext from '../../context/contact/contactContext';
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
-    const contactContext = useContext(contactContext);
+    const contactContext = useContext(ContactContext);
+    const text = useRef(' ');
+
+    const { filterContacts, clearFilter, filtered } = contactContext;
+
+    useEffect(() => {
+        if (filtered === null) {
+            text.current.value = '';
+        }
+    });
+
+    const onChange = e => {
+        if (text.current.value !== '') {
+            filterContacts(e.target.value);
+        } else {
+            clearFilter();
+        }
+    };
+
     return (
-        <form
-            type='text'
-            ref={text}
-            placeholder='Filter contacts'
-            onChange={onChage}
-        />
+        <form>
+            <input
+                type='text'
+                ref={text}
+                placeholder='Filter Contacts...'
+                onChange={onChange}
+            />
+        </form>
     );
 };
 
